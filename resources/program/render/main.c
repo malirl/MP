4e1b540c5b6181a76092b6627dfbcb7e122656a2
Adam <astromalir@gmail.com>
2022-10-22 14:08:04 +0200

    konecne si po sobe uklidil

---
+++ resources/program/render/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+#define WIDTH 1000
+#define HEIGHT 1000
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Texture *screen_texture;
+
+unsigned int *pixels;
+
+int check_events() {
+   SDL_Event event;
+   while (SDL_PollEvent(&event))
+   {
+      switch (event.type) {
+         case SDL_QUIT:
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            break;
+         case SDL_WINDOWEVENT:
+            break;
+         default: {}
+      }
+   }
+}
+
+int check_events_forever() {
+   while(1) {
+      SDL_Event event;
+      while (SDL_PollEvent(&event))
+      {
+         switch (event.type) {
+            case SDL_QUIT:
+               SDL_DestroyWindow(window);
+               SDL_Quit();
+               break;
+            case SDL_WINDOWEVENT:
+               break;
+            default: {}
+         }
+      }
+   }
+}
+
+void render_frame() {
+   SDL_UpdateTexture(screen_texture, NULL, pixels, WIDTH * 4);
+   SDL_RenderClear(renderer);
+   SDL_RenderCopy(renderer, screen_texture, NULL, NULL);
+   SDL_RenderPresent(renderer);
+   check_events();
+}
+
+
+
+void set_pixel(int x, int y) {
+   pixels[x + (HEIGHT - y) * WIDTH] = 0xff000000;
+}
+
+int init() {
+   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+      fprintf(stderr, "Could not initialize sdl2: %s\n", SDL_GetError());
+      return EXIT_FAILURE;
+   }
+
+   window = SDL_CreateWindow("",
+         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+         WIDTH, HEIGHT,
+         SDL_WINDOW_SHOWN);
+
+
+   if(window == NULL) {
+      fprintf(stderr,"SDL_CreateWindow Error: %s\n", SDL_GetError());
+      return EXIT_FAILURE;
+   }
+
+   renderer = SDL_CreateRenderer(window, -1,
+         SDL_RENDERER_ACCELERATED |
+         SDL_RENDERER_PRESENTVSYNC);
+
+   if(renderer == NULL) {
+      SDL_DestroyWindow(window);
+      fprintf(stderr, "SD1000L_CreateRenderer Error: %s\n", SDL_GetError());
+      return EXIT_FAILURE;
+   }
+
+   screen_texture = SDL_CreateTexture(renderer,
+         SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING,
+         WIDTH, HEIGHT);
+
+   pixels = malloc(WIDTH * HEIGHT * 4);
+}
+
