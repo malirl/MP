2ad7b99024df8821bfe1cf618908dd706924f558
Adam <astromalir@gmail.com>
2022-10-22 14:08:58 +0200

    ,...temer

--- resources/program/line/main.c
+++ resources/program/line/main.c
@@ -1,95 +1,103 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <SDL2/SDL.h>
 
-#define WIDTH 1000
-#define HEIGHT 1000
+typedef struct{
+ int ax, ay, bx, by;
+}input;
 
-SDL_Window *window;
-SDL_Renderer *renderer;
-SDL_Texture *screen_texture;
+/* v poradi: bod A, bod B vuci A, parametr pro zpetny prevod */
+typedef struct{
+   int ax, ay, dx, dy, opt;
+   double a;
+}res;
 
-unsigned int *pixels;
+typedef struct{
+   int *x, *y;
+}point;
 
-int check_events() {
-   SDL_Event event;
-   while (SDL_PollEvent(&event))
-   {
-      switch (event.type) {
-         case SDL_QUIT:
-            SDL_DestroyWindow(window);
-            SDL_Quit();
-            break;
-         case SDL_WINDOWEVENT:
-            break;
-         default: {}
-      }
-   }
-}
+typedef struct{
+   point mirror;
+   int bx, by;
+}pointToMirror;
 
-int check_events_forever() {
-   while(1) {
-      SDL_Event event;
-      while (SDL_PollEvent(&event))
-      {
-         switch (event.type) {
-            case SDL_QUIT:
-               SDL_DestroyWindow(window);
-               SDL_Quit();
-               break;
-            case SDL_WINDOWEVENT:
-               break;
-            default: {}
-         }
-      }
-   }
-}
+point Point;
+input Input;
 
-void render_frame() {
-   SDL_UpdateTexture(screen_texture, NULL, pixels, WIDTH * 4);
-   SDL_RenderClear(renderer);
-   SDL_RenderCopy(renderer, screen_texture, NULL, NULL);
-   SDL_RenderPresent(renderer);
-   check_events();
+res convert_input(input *input);
+
+void init_bresenham(res *res, point *point);
+void init_dda(res *res, point *point);
+int next_point(void);
+
+void init(void);
+void set_pixel(int x, int y);
+
+void render_frame(void);
+int check_events_forever(void);
+
+void mirror(pointToMirror *B) {
+   int px = Input.ax, py = Input.ay, bx = B->bx, by = B->by;
+   int x = Input.bx-Input.ax, y = Input.by-Input.ay; 
+
+   double k = (double)(x*(bx-px)+y*(by-py))/(x*x+y*y);
+
+   int res_x = (int)2*(k*x+px)-bx+.5, res_y = (int)2*(k*y+py)-by+.5;
+
+   B->mirror.x = &res_x;
+   B->mirror.y = &res_y;
 }
 
 
 
-void set_pixel(int x, int y) {
-   pixels[x + (HEIGHT - y) * WIDTH] = 0xff000000;
-}
+int main() {
+   init();
 
-int init() {
-   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-      fprintf(stderr, "Could not initialize sdl2: %s\n", SDL_GetError());
-      return EXIT_FAILURE;
+   Input.ax = 100;
+   Input.ay = 300;
+   Input.bx = 400;
+   Input.by = 850;
+
+   res res = convert_input(&Input);
+   init_bresenham(&res, &Point);
+   /* init_dda(&res, &Point); */
+
+
+   /* zrcadleni */
+   pointToMirror B;
+   B.bx = 410;
+   B.by = 400;
+
+   set_pixel(B.bx,B.by);
+   mirror(&B);
+   set_pixel(*B.mirror.x,*B.mirror.y);
+   printf("%f",*B.mirror.x);
+
+
+
+   switch(res.opt) {
+      case 1:
+         while(next_point())
+            set_pixel(res.ax + *Point.y, res.ay + *Point.x);
+         break;
+      case 2:
+         while(next_point())
+            set_pixel(res.ax + *Point.x, res.ay + *Point.y);
+         break;
+      case 3:
+         while(next_point())
+            set_pixel(res.ax + *Point.x, res.ay - *Point.y);
+         break;
+      case 4:
+         while(next_point())
+            set_pixel(res.ax + *Point.y, res.ay - *Point.x);
+         break;
    }
 
-   window = SDL_CreateWindow("",
-         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-         WIDTH, HEIGHT,
-         SDL_WINDOW_SHOWN);
 
 
-   if(window == NULL) {
-      fprintf(stderr,"SDL_CreateWindow Error: %s\n", SDL_GetError());
-      return EXIT_FAILURE;
-   }
+   render_frame();
+   check_events_forever();
 
-   renderer = SDL_CreateRenderer(window, -1,
-         SDL_RENDERER_ACCELERATED |
-         SDL_RENDERER_PRESENTVSYNC);
-
-   if(renderer == NULL) {
-      SDL_DestroyWindow(window);
-      fprintf(stderr, "SD1000L_CreateRenderer Error: %s\n", SDL_GetError());
-      return EXIT_FAILURE;
-   }
-
-   screen_texture = SDL_CreateTexture(renderer,
-         SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING,
-         WIDTH, HEIGHT);
-
-   pixels = malloc(WIDTH * HEIGHT * 4);
+   getchar();
+   return 0;
 }
 
